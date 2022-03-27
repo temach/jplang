58373805c0715333118e4f8cfbcea9ec3176223d
Artem Abramov <tematibr@gmail.com>
2022-03-27 21:24:08 +0300

    WIP iframe click-through

--- moonspeak/test/iframe-clickthrough/frame_alert.js
+++ moonspeak/test/iframe-clickthrough/frame_alert.js
@@ -1,6 +1,5 @@
 alert('Alert script executing');
 console.log(document);
-console.log(appRootElement);
 document.getElementById('alert_button').addEventListener('click', (e) => {
      alert("Hello! This is an alert box");
 });
