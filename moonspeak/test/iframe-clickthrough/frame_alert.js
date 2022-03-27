747521bb7d66fef71f837d2e7dc706147ac8317d
Artem Abramov <tematibr@gmail.com>
2022-03-27 10:07:00 +0300

    Poorly implemented javascript running in shadow dom

--- moonspeak/test/iframe-clickthrough/frame_alert.js
+++ moonspeak/test/iframe-clickthrough/frame_alert.js
@@ -1,3 +1,6 @@
+alert('Alert script executing');
+console.log(document);
+console.log(appRootElement);
 document.getElementById('alert_button').addEventListener('click', (e) => {
      alert("Hello! This is an alert box");
 });
