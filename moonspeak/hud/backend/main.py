fc92da169426488875b5337189420803f3a619a0
Artem Abramov <tematibr@gmail.com>
2022-03-28 23:56:33 +0300

    Hud: combines the main web ui features

---
+++ moonspeak/hud/backend/main.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+import os
+import json
+import sqlite3
+import re
+import uuid
+from urllib.parse import urlparse
+
+from fastapi import FastAPI
+from fastapi.requests import Request
+from fastapi.responses import Response, FileResponse
+import uvicorn
+import requests
+
+VERSION = "0.1"
+DB_PATH = "../tmp/hud_features.db"
+DB = sqlite3.connect(DB_PATH)
+MAPPING = {}
+
+app = FastAPI()
+
+
+@app.api_route("/api/routing/{fid}/{furl:path}", methods=["GET", "HEAD", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"])
+async def routing(request: Request, fid, furl: str):
+    print(f'Request {furl} of feature {fid}')
+    feature_root_url = MAPPING[fid]
+    url = feature_root_url + furl
+    r = requests.request(request.method, url, headers=request.headers, data=await request.body())
+    print(f'Requested {r.url}')
+    return Response(content=r.content, status_code=r.status_code, headers=r.headers)
+
+
+@app.get("/")
+def index():
+    return static("index.html")
+
+
+@app.get("/static/{path:path}")
+def static(path):
+    if "index.html" in path:
+        p = os.path.join("..", "frontend", path)
+    else:
+        p = os.path.join("..", "frontend", "static", path)
+
+    if os.path.exists(p):
+        return FileResponse(p)
+    else:
+        return Response(status_code=404)
+
+
+@app.get("/api/getfeature")
+def feature(feature_url: str):
+    # feature_url gets automatically inferred as url query parameter by FastAPI
+    r = requests.get(feature_url)
+
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    fid = str(uuid.uuid4())
+    base_tag = soup.new_tag("base", href="api/routing/{}/".format(fid))
+    soup.head.insert(0, base_tag)
+
+    # unify url representation
+    MAPPING[fid] = urlparse(feature_url).geturl()
+
+    resp = {
+        "text": str(soup),
+    }
+    return resp
+
+
+def db_init():
+    c = DB.cursor()
+    c.execute("""CREATE TABLE hud_features (
+            id TEXT NOT NULL UNIQUE
+            , url TEXT NOT NULL UNIQUE
+            , notes TEXT
+            , PRIMARY KEY (id, url)
+        );
+        """)
+    DB.commit()
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Feature, run as "python main.py"')
+    parser.add_argument('port', type=int, help='port number')
+    args = parser.parse_args()
+
+    db_needs_init = (not os.path.isfile(DB_PATH)) or (
+        os.path.getsize(DB_PATH) == 0)
+
+    if db_needs_init:
+        db_init()
+
+    if not db_needs_init:
+        # if db already existed, read some data from it
+        c = DB.cursor()
+        c.execute("SELECT * FROM hud_features")
+        rows = c.fetchall()
+        for r in rows:
+            name, url, notes = r
+            print(r)
+
+    print("Running server on port {}".format(args.port))
+    import logging
+    uvicorn.run(app, host="0.0.0.0", port=args.port, debug=True)
---
+++ moonspeak/hud/frontend/static/main.js
@@ -0,0 +1,96 @@
+
+async function get_feature(feature_url) {
+    let backend = new URL("/api/getfeature", window.location);
+    backend.searchParams.set('feature_url', new URL(feature_url));
+
+    let response = await fetch(backend);
+    if (!response.ok) {
+        throw new Error("HTTP error, status = " + response.status);
+    }
+    let feature_json = await response.json();
+    return feature_json;
+}
+
+async function loadHudFeatures() {
+    let urls = [
+        "http://0.0.0.0:9010",
+        "http://0.0.0.0:9011",
+        "http://0.0.0.0:9012",
+    ];
+
+    let container = document.getElementById("featuresContainer");
+
+    let fullscreenFrames = new Array();
+    for (const url of urls) {
+        try {
+            let feature_json = await get_feature(url);
+            // dublicate requests is a known bug: https://bugzilla.mozilla.org/show_bug.cgi?id=1464344
+            let iframe = document.createElement("iframe");
+            iframe.classList.add("fullscreen");
+            iframe.srcdoc = feature_json["text"];
+
+            container.appendChild(iframe);
+            fullscreenFrames.push(iframe);
+        } catch (error) {
+            console.log("HTTP error:" + error.message);
+        };
+    }
+
+    return fullscreenFrames;
+
+}
+
+async function setupHandlers(fullscreenFrames) {
+    let STATE_PROPOGATING = "propogating";
+    let STATE_HANDLED = "handled";
+
+    let eventTrap = document.getElementById("eventTrap");
+
+    let handler = (e) => {
+        let evt = new MouseEvent(e.type, e);
+        evt.moonspeakEventState = STATE_PROPOGATING;
+        for (const child of fullscreenFrames) {
+                if (evt.moonspeakEventState === STATE_HANDLED) {
+                    // stop early if event was handled
+                    break; 
+                }
+                let innerEl = child.contentWindow.document.elementFromPoint(e.clientX, e.clientY);
+                if (innerEl) {
+                    innerEl.dispatchEvent(evt);
+                }
+        };
+    }
+
+    eventTrap.addEventListener('click', handler, true);
+
+    for (const iframeElem of fullscreenFrames) {
+        let iframeHtml = iframeElem.contentWindow.document.documentElement;
+        iframeHtml.addEventListener('click', (e) => {
+            if (e.target !== iframeHtml && (! e.target.classList.contains('deadzone'))) {
+                // if target element: is not document.html tag and not deadzone css class
+                // means user clicked on something worthwhile (likely with a lower click event handler)
+                e.moonspeakEventState = STATE_HANDLED;
+                eventTrap.classList.remove('active');
+                iframeElem.classList.add('active');
+            } else if (iframeElem.classList.contains('active')) {
+                // user clicked on non-active element, so current iframe should stop being active
+                iframeElem.classList.remove('active');
+                eventTrap.classList.add('active');
+                let evt = new MouseEvent('click', e);
+                eventTrap.dispatchEvent(evt);
+            }
+        }, true);
+    };
+}
+
+async function initHud() {
+    let fullscreenFrames = await loadHudFeatures();
+
+    // background element is first child of container (to put it in the bottom of hierarchy)
+    // but background element needs to be the LAST to check when assigning event handlers
+    // so reverse list of iframes, to place background last
+    fullscreenFrames.reverse();
+
+    setupHandlers(fullscreenFrames);
+}
+
