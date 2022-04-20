30ccc5f3d62a21ae511b9158a20efbfb06edd9a4
Artem Abramov <tematibr@gmail.com>
2022-04-20 16:11:56 +0300

    Simplify backend code

--- moonspeak/router-bottle/backend/main.py
+++ moonspeak/router-bottle/backend/main.py
@@ -3,73 +3,61 @@ import os
 import json
 import sqlite3
 import re
-from urllib.parse import urlparse, quote_plus, unquote_plus
+from urllib.parse import unquote_plus
 
 from bottle import route, run, get, static_file, request, HTTPResponse
-
 import requests
 
 VERSION = "0.1"
 DB_PATH = "../tmp/hud_features.db"
 DB = sqlite3.connect(DB_PATH)
-MAPPING = {}
-
 MY_HOSTNAME = None
 
-# def retrieve_url(url):
+
+def modify_root_doc(doc_text, fid):
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(doc_text, 'html.parser')
+    base_tag = soup.new_tag("base", href="http://{}/api/routing/{}/".format(MY_HOSTNAME, fid))
+    soup.head.insert(0, base_tag)
+    return str(soup)
 
 
-@route("/api/routing/<fid>", method=["GET"])
-def routing_root(fid):
-    feature_root_url = unquote_plus(fid)
-    url = feature_root_url
-
+def retrieve_url(url, req):
     print(f"Requesting {url}")
 
-    # request.headers is read-only so we create a new object to pass to requests
+    # in bottle request.headers is read-only so we create a new object to pass to requests
     # Host must be deleted so network transport can infer the correct Host header from url
     # Content-Length must be deleted because bottle always includes it (in case of POST its set to '')
     bad_headers = frozenset(('Host', 'Content-Length'))
-    headers = {k: v for k,v in request.headers.items() if k not in bad_headers}
+    headers = {k: v for k,v in req.headers.items() if k not in bad_headers}
 
-    r = requests.request(request.method, url, headers=headers, data=request.body)
-
-    # modify returned content because we are requesting root doc
-    new_content = modify_root_doc(r.content, fid)
-
-    # delete content-lenght so it will get recalculated
-    del r.headers["content-length"]
+    r = requests.request(req.method, url, headers=headers, data=req.body)
 
     # remove hop-by-hop headers
     # see: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection
     # see: https://datatracker.ietf.org/doc/html/rfc2616.html#section-13.5.1
     # list of hop-by-hop headers: https://github.com/python/cpython/blob/main/Lib/wsgiref/util.py#L151
     del r.headers["connection"]
+
+    return r
+
+
+@route("/api/routing/<fid>", method=["GET"])
+def routing_root(fid):
+    r = retrieve_url(unquote_plus(fid), request)
+
+    # modify returned content because we are requesting root doc
+    new_content = modify_root_doc(r.content, fid)
+    # delete content-lenght so it will get recalculated
+    del r.headers["content-length"]
 
     return HTTPResponse(body=new_content, status=r.status_code, headers=r.headers.items())
 
 
 @route("/api/routing/<fid>/<furl:re:.*>", method=["GET", "HEAD", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"])
 def routing(fid, furl):
-    feature_root_url = unquote_plus(fid)
-    url = feature_root_url + "/" + furl
-
-    print(f"Requesting {url}")
-
-    # request.headers is read-only so we create a new object to pass to requests
-    # Host must be deleted so network transport can infer the correct Host header from url
-    # Content-Length must be deleted because bottle always includes it (in case of POST its set to '')
-    bad_headers = frozenset(('Host', 'Content-Length'))
-    headers = {k: v for k,v in request.headers.items() if k not in bad_headers}
-
-    r = requests.request(request.method, url, headers=headers, data=request.body)
-
-    # remove hop-by-hop headers
-    # see: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection
-    # see: https://datatracker.ietf.org/doc/html/rfc2616.html#section-13.5.1
-    # list of hop-by-hop headers: https://github.com/python/cpython/blob/main/Lib/wsgiref/util.py#L151
-    del r.headers["connection"]
-
+    url = unquote_plus(fid) + "/" + furl
+    r = retrieve_url(url, request)
     return HTTPResponse(body=r.content, status=r.status_code, headers=r.headers.items())
 
 
@@ -88,35 +76,10 @@ def index():
 
     return static_file("index.html", root="../frontend/")
 
+
 @get("/static/<filepath:re:.*\.(css|js)>")
 def static(filepath):
     return static_file(filepath, root="../frontend/static/")
-
-def modify_root_doc(doc_text, fid):
-    from bs4 import BeautifulSoup
-    soup = BeautifulSoup(doc_text, 'html.parser')
-    base_tag = soup.new_tag("base", href="http://{}/api/routing/{}/".format(MY_HOSTNAME, fid))
-    soup.head.insert(0, base_tag)
-    print(f"Soup for fid __{fid}__:")
-    print(str(soup))
-    return str(soup)
-
-# @get("/api/getfeature")
-# def feature():
-#     feature_url = request.params.get('feature_url')
-#     # fid = re.sub(r'[\\/*?:"<>|+=.& ]', "_", feature_url)
-#     fid = quote_plus(feature_url)
-#     print(f"fid: __{fid}__")
-#     # add mapping with unified url representation
-#     MAPPING[fid] = urlparse(feature_url).geturl()
-# 
-#     src = "http://{}/api/routing/{}/".format(MY_HOSTNAME, fid)
-#     print(f"src: __{src}__")
-#     # return root URL for this feature
-#     return { 
-#         "src" : src,
-#         "text" : "use src",
-#     }
 
 
 def db_init():
