650f4d7c044f06a437ded39583c88fa62e04a052
Artem Abramov <tematibr@gmail.com>
2022-06-20 08:29:28 +0300

    Fix: drop previous images BEFORE upgrading and creating fallback ones

--- moonspeak/ru-upgrade-docker-compose.sh
+++ moonspeak/ru-upgrade-docker-compose.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
+sudo docker image prune --force
 sudo docker-compose pull
 sudo docker-compose -f docker-compose.yml -f docker-compose.override.ru.yml up -d
-sudo docker image prune --force
