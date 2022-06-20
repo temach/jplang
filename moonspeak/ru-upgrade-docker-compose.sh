0414e8604323ffcbfc9bdfb2dfadf8feb3a0a2d8
Artem Abramov <tematibr@gmail.com>
2022-06-20 09:33:20 +0300

    Add steps to upgrade docker-compose definitions if needed

--- moonspeak/ru-upgrade-docker-compose.sh
+++ moonspeak/ru-upgrade-docker-compose.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 sudo docker image prune --force
+
+# optionally update the docker-compose definitions
+# sudo docker-compose down
+# rm docker-compose.override.ru.yml 
+# rm docker-compose.yml 
+# wget https://raw.githubusercontent.com/temach/jplang/master/moonspeak/docker-compose.override.ru.yml
+# wget https://raw.githubusercontent.com/temach/jplang/master/moonspeak/docker-compose.yml
+# cat docker-compose.override.ru.yml 
+# cat docker-compose.yml 
+
 sudo docker-compose pull
 sudo docker-compose -f docker-compose.yml -f docker-compose.override.ru.yml up -d
