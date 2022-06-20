f3b7a9322c049ce23a8590f6f7b23ff351a43e07
Artem Abramov <tematibr@gmail.com>
2022-06-20 08:57:41 +0300

    Script to renew lets encrypt certificate

---
+++ moonspeak/certbot-lets-encrypt.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# see: https://eff-certbot.readthedocs.io/en/stable/install.html#running-with-docker
+
+sudo docker-compose down
+
+# create certificate with this:
+# sudo docker run -it --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 certbot/certbot certonly
+
+# renew certificate with this:
+sudo docker run -it --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 certbot/certbot renew
+
+sudo docker-compose -f docker-compose.yml -f docker-compose.override.ru.yml up -d
