c02aeb02d4c12e4d6c5619658dd82e52e2fe4eb8
Artem Abramov <tematibr@gmail.com>
2022-07-01 14:33:52 +0300

    Using actix_web for server and rust libcurl bindings for requests, but should change libcurl for awc - actix web client

--- moonspeak/router/src/main.rs
+++ moonspeak/router/src/main.rs
@@ -1,23 +1,82 @@
 #[macro_use]
 extern crate log;
+use std::env;
+use url::{Url, ParseError};
+use curl::easy::Easy;
+use curl::easy::List;
 use actix_web::{get, web, App, HttpServer, HttpResponse, HttpRequest, middleware::Logger};
 
 
-fn router(req: HttpRequest, node: String, service: String, path: String) -> String {
+// This struct represents state
+struct AppState {
+    domain: String,
+}
+
+fn router(
+    req: HttpRequest,
+    node: String, 
+    service: String, 
+    path: String, 
+    appstate: web::Data<AppState>, 
+    body: web::Bytes,
+) -> Result<String, Box<dyn std::error::Error>> {
     info!("Request: {:?}", req);
 
-    format!("Welcome {}! {} to the url: {}", node, service, path)
+    // figure out scheme and netloc: unix sock or TCP
+    let scheme = "http";
+    let netloc = format!("{}.{}", service, &appstate.domain);
+
+
+    // Make target url
+    let base_str = format!("{}://{}", scheme, netloc.as_str());
+    let base = Url::parse(base_str.as_str())?;
+    let url = base.join(path.as_str())?.join(req.query_string())?;
+    info!("Requesting {} {:?}", req.method(), url.as_str());
+
+    // Make target headers
+    let mut headers = List::new();
+    // a Set-Cookie can appear 3 times, then iterator will give three pairs of (header, value)
+    for (header, value) in req.headers().iter() {
+        let s = format!("{}: {}", header, value.to_str()?);
+        headers.append(s.as_str()).unwrap();
+    }
+    info!("Requesting headers {:?}", headers);
+
+    let mut handle = Easy::new();
+    handle.custom_request(req.method().as_str());
+    handle.url(url.as_str()).unwrap();
+    handle.http_headers(headers).unwrap();
+
+    // let mut transfer = handle.transfer();
+    // transfer.read_function(|into| {
+    //     let byteslice: &[u8] = &body[..];
+    //     Ok(byteslice.read(into).unwrap())
+    // }).unwrap();
+    // Ok(transfer)
+
+    let res = format!("Welcome {}! {} to the url: {}", node, service, path);
+    Ok(res)
 }
 
 
-async fn handler2(req: HttpRequest, combined_path: web::Path<(String, String)>) -> String {
+async fn handler2(
+    req: HttpRequest,
+    combined_path: web::Path<(String, String)>,
+    body: web::Bytes,
+    appstate: web::Data<AppState>, 
+) -> String {
     let (node, service) = combined_path.into_inner();
-    router(req, node, service, String::new())
+    router(req, node, service, String::new(), appstate, body).unwrap()
 }
 
-async fn handler3(req: HttpRequest, combined_path: web::Path<(String, String, String)>) -> String {
+async fn handler3(
+    req: HttpRequest,
+    combined_path: web::Path<(String, String, String)>, 
+    body: web::Bytes,
+    appstate: web::Data<AppState>
+) -> String {
     let (node, service, path) = combined_path.into_inner();
-    router(req, node, service, path)
+    router(req, node, service, path, appstate, body).unwrap()
 }
 
 
@@ -29,9 +88,12 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
+            .app_data(web::Data::new(AppState {
+                domain: env::var("MOONSPEAK_DOMAIN").unwrap_or("moonspeak.test".to_string()),
+            }))
             .wrap(Logger::default())
             .route("/router/{node}/{service}", web::to(handler2))
-            .route("/router/{node}/{service}/{other_url:.*}", web::to(handler3))
+            .route("/router/{node}/{service}/{path:.*}", web::to(handler3))
     })
     .bind(("0.0.0.0", 8080))?
     .bind_uds("./here.sock")?
