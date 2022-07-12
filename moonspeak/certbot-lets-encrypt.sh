6887489ac158d97aca46b760bd12fe6fd9bf9e3c
Artem Abramov <tematibr@gmail.com>
2022-07-12 18:54:47 +0300

    now certbot script forces renewal

--- moonspeak/certbot-lets-encrypt.sh
+++ moonspeak/certbot-lets-encrypt.sh
@@ -8,6 +8,6 @@ sudo docker-compose down
 # sudo docker run -it --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 certbot/certbot certonly
 
 # renew certificate with this:
-sudo docker run -it --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 certbot/certbot renew
+sudo docker run -it --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 certbot/certbot renew --force-renewal
 
 sudo docker-compose -f docker-compose.yml -f docker-compose.override.ru.yml up -d
