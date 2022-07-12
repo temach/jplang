acda9ab39063125f76b5b46a7546b306175aee32
Artem Abramov <tematibr@gmail.com>
2022-07-12 19:42:03 +0300

    Fix upgrade scripts for docker

--- moonspeak/certbot-lets-encrypt.sh
+++ moonspeak/certbot-lets-encrypt.sh
@@ -2,12 +2,12 @@
 
 # see: https://eff-certbot.readthedocs.io/en/stable/install.html#running-with-docker
 
-sudo docker-compose down
+docker-compose down
 
 # create certificate with this:
-# sudo docker run -it --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 certbot/certbot certonly
+# docker run -it --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 certbot/certbot certonly
 
 # renew certificate with this:
-sudo docker run -it --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 certbot/certbot renew --force-renewal
+docker run -it --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 certbot/certbot renew --force-renewal
 
-sudo docker-compose -f docker-compose.yml -f docker-compose.override.ru.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose.override.ru.yml up -d
