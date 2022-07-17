222783dc0302ff0e06ad9498ba6cac89d3b2c7ea
Artem Abramov <tematibr@gmail.com>
2022-07-17 16:08:35 +0300

    Simpler resetting of document.domain
    Works only when running on same host with different ports.
    e.g. moonspeak.test:8001, moonspeak.test:8002
    or localhost:8001, localhost:8002

--- moonspeak/grapheditor/frontend/js/dev_mode.js
+++ moonspeak/grapheditor/frontend/js/dev_mode.js
@@ -1,8 +1,6 @@
 window.addEventListener("load", moonspeakDevMode, true);
 
 function moonspeakDevMode() {
-    // reset domain to allow cross-domain iframe access while in dev mode
-    let temp = location.hostname.split('.').reverse();
-    let root_domain = temp[1] + '.' + temp[0];
-    document.domain = root_domain;
+    // reset domain (sets port to null) to allow cross-domain iframe access while in dev mode
+    document.domain = location.hostname;
 }
