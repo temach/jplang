f90fa3deb02d4777b57c96f454025173050cc81d
Artem Abramov <tematibr@gmail.com>
2022-07-31 16:16:06 +0300

    workelements: working dockerfile

--- moonspeak/workelements/backend/gunicorn.conf.py
+++ moonspeak/workelements/backend/gunicorn.conf.py
@@ -1,7 +1,7 @@
 # config priorities see: https://docs.gunicorn.org/en/latest/configure.html
 # config options see: https://docs.gunicorn.org/en/latest/settings.html
 
-bind=["127.0.0.1:8040"]
+bind=["0.0.0.0:8040"]
 
 # see: https://www.joelsleppy.com/blog/gunicorn-application-preloading/
 preload_app = True
