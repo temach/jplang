2fc81790345e356b913574089a3b320a78b10c71
Artem Abramov <tematibr@gmail.com>
2022-09-04 21:40:04 +0300

    wget uses no-cache option when getting docker-compose from github CDN

--- moonspeak/ru-upgrade-docker-compose.sh
+++ moonspeak/ru-upgrade-docker-compose.sh
@@ -5,8 +5,8 @@ docker image prune --force
 docker-compose down
 rm docker-compose.override.ru.yml 
 rm docker-compose.yml 
-wget https://raw.githubusercontent.com/temach/jplang/master/moonspeak/docker-compose.override.ru.yml
-wget https://raw.githubusercontent.com/temach/jplang/master/moonspeak/docker-compose.yml
+wget --no-cache https://raw.githubusercontent.com/temach/jplang/master/moonspeak/docker-compose.override.ru.yml
+wget --no-cache https://raw.githubusercontent.com/temach/jplang/master/moonspeak/docker-compose.yml
 cat docker-compose.override.ru.yml 
 cat docker-compose.yml 
 
