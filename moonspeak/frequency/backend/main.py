edea2b31bf6f25b3c699f08501c1414fabb700ce
JustDOOVE <dove.golub.dog@gmail.com>
2023-02-20 15:39:13 +0600

    frequency: add docker file

--- moonspeak/frequency/backend/main.py
+++ moonspeak/frequency/backend/main.py
@@ -2,7 +2,7 @@ from bottle import route, run, static_file, request
 import json
 import string
 from collections import defaultdict
-
+import os
 
 
 nums_ords = [i for i in range(48, 58)]
@@ -30,4 +30,15 @@ def submit():
         'ISO-8859-1').decode('utf-8')
     return json.dumps(frequency(user_string.lower()), ensure_ascii=False)
 
-run(host='localhost', port=8080, debug=True)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Feature, run as "python main.py"')
+    parser.add_argument('--port', type=int, default=os.getenv("MOONSPEAK_PORT", 8005), help='port number')
+    args = parser.parse_args()
+
+    # other
+    print("Running bottle server on port {}".format(args.port))
+    run(host='0.0.0.0', port=args.port)
