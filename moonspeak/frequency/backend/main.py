107389e0e2bf06d0154852b5c677ce9329243d72
JustDOOVE <dove.golub.dog@gmail.com>
2023-04-12 13:56:20 +0600

    frequency: now module can read text from url
    frequency: added a module for getting japanese characters from url.

--- moonspeak/frequency/backend/main.py
+++ moonspeak/frequency/backend/main.py
@@ -2,6 +2,7 @@ from bottle import route, run, static_file, request, response
 import json
 from collections import Counter
 import os
+from requests_html import HTMLSession
 
 latin_ords = set(i for i in range(97, 123))
 japan_ords = set(i for i in range(19969, 40959))
@@ -9,9 +10,20 @@ all_ords = latin_ords | japan_ords
 
 
 def frequency(user_string: str):
-    result = Counter(user_string)
-    sorted_result = result.most_common()
-    return {k: v for k, v in sorted_result if ord(k) in all_ords}
+    try:
+        session = HTMLSession()
+        parse = session.get(user_string)
+        parse.html.render(timeout=40)
+        html_page_text = parse.html.html
+        list_japan_chars_from_url = [i for i in html_page_text if ord(i) in japan_ords]
+        japan_chars_from_url = {i: list_japan_chars_from_url.count(i) for i in set(list_japan_chars_from_url)}
+        result = {key: value for key, value in sorted(japan_chars_from_url.items(), key=lambda item: item[1], reverse=True)}
+        return result
+    except:
+        result = Counter(user_string)
+        sorted_result = result.most_common()
+        return {k: v for k, v in sorted_result if ord(k) in all_ords}
+    
 
 @route("/")
 def index():
